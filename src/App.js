b28ea513b7a4738471f5092d05bb0f3156ebf434
Hg Nam <namlai2119@gmail.com>
2023-02-23 13:35:38 +0700

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const oders = [100, 200, 300]
 
 function App() {
+
+  const [counter, setCounter] = useState(1);
+
+  const handleIncrease = () => {
+    setCounter(preStare => preStare + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
