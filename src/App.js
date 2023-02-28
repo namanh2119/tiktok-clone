20577d3774a1ff37e095a5eb72d93e403fe9b3e5
Hg Nam <namlai2119@gmail.com>
2023-02-28 23:15:14 +0700

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { useState } from "react";
-
-const oders = [100, 200, 300]
 
 function App() {
 
-  const [counter, setCounter] = useState(1);
-
-  const handleIncrease = () => {
-    setCounter(preStare => preStare + 1);
-  }
-
   return (
-    <div className="App">
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
+    <div>
+      
     </div>
-  );
+  )
 }
 
 export default App;
