013f15eaba3c0ea545f661c4f2000a769ba2ecbf
Hg Nam <namlai2119@gmail.com>
2023-03-04 00:30:18 +0700

    modified:   package-lock.json 	modified:   package.json 	new file:   src/assets/images/index.js 	new file:   src/assets/images/logo.svg 	modified:   src/components/GlobalStyles/GlobalStyles.scss 	new file:   src/components/Layout/DefaultLayout/DefaultLayout.module.scss 	new file:   src/components/Layout/DefaultLayout/Sidebar/Sidebar.module.scss 	modified:   src/components/Layout/DefaultLayout/Sidebar/index.js 	modified:   src/components/Layout/DefaultLayout/index.js 	modified:   src/components/Layout/HeaderOnly/index.js 	new file:   src/components/Layout/components/Header/Header.module.scss 	modified:   src/components/Layout/components/Header/index.js

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,32 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="search accounts and videos" spellCheck="false" />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('icon-search')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
