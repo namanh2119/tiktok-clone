68c1edfaebade3dcf23ba77ed7c0602459cbc322
Hg Nam <namlai2119@gmail.com>
2023-03-07 23:23:52 +0700

    modified:   package-lock.json 	modified:   package.json 	new file:   src/components/AccountItem/AccountItem.module.scss 	new file:   src/components/AccountItem/index.js 	modified:   src/components/GlobalStyles/GlobalStyles.scss 	modified:   src/components/Layout/components/Header/Header.module.scss 	modified:   src/components/Layout/components/Header/index.js 	new file:   src/components/Layout/components/Popper/Popper.module.scss 	new file:   src/components/Layout/components/Popper/Wrapper.js 	new file:   src/components/Layout/components/Popper/index.js

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://haycafe.vn/wp-content/uploads/2022/03/Anh-anime-nu-anh-anime-girl.jpg"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <h4>Nguyễn Mai Hoa</h4>
+                    <FontAwesomeIcon icon={faCheckCircle} className={cx('check')} />
+                </p>
+                <span className={cx('usename')}>Nguyễn Mai Hoa</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
